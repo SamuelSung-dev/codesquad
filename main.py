78f658cc539a20161535d378f542f89192b0b563
Samuel Sung <SamuelSung.dev@gmail.com>
2020-12-07 21:07:04 +0900

    Parse input to make action

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ cube = Cube2d()
 cube.show()
 
 while True:
-    print("\nCUBE>", end="")
+    print("\nCUBE> ", end="")
     input_text = input()
 
-    if input_text == "Q":
-        print("Bye~")
-        break
+    print()
+    action = []
+    for ii in range(len(input_text)):
+        letter = input_text[ii]
+        letter_next = ""
+        try:
+            letter_next = input_text[ii+1]
+        except:
+            letter_next = ""
+        if letter == "U" or letter == "R" or letter == "L" or letter == "B":
+            action_tmp = letter
+            if letter_next == "'":
+                action_tmp = action_tmp+"'"
+            action.append(action_tmp)
+        elif letter == "Q":
+            action.append(letter)
+
+    print(action)

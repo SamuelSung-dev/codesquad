ac6b500fc2928681599752a84561b60af81c339c
Samuel Sung <SamuelSung.dev@gmail.com>
2020-12-07 21:50:10 +0900

    Add cube control code

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from cube2d import Cube2d
 cube = Cube2d()
 
 cube.show()
+print()
 
 while True:
-    print("\nCUBE> ", end="")
+    print("CUBE> ", end="")
     input_text = input()
 
-    print()
-    action = []
+    actions = []
     for ii in range(len(input_text)):
         letter = input_text[ii]
         letter_next = ""
@@ -21,8 +21,32 @@ while True:
             action_tmp = letter
             if letter_next == "'":
                 action_tmp = action_tmp+"'"
-            action.append(action_tmp)
+            actions.append(action_tmp)
         elif letter == "Q":
-            action.append(letter)
+            actions.append(letter)
 
-    print(action)
+    for action in actions:
+        if action == "Q":
+            print("Bye~")
+            exit()
+
+        print()
+        print(action)
+        if action == "U":
+            cube.rotate_top_left()
+        elif action == "U'":
+            cube.rotate_top_right()
+        elif action == "R":
+            cube.rotate_right_up()
+        elif action == "R'":
+            cube.rotate_right_down()
+        elif action == "L":
+            cube.rotate_left_down()
+        elif action == "L'":
+            cube.rotate_left_up()
+        elif action == "B":
+            cube.rotate_bottom_right()
+        elif action == "B'":
+            cube.rotate_bottom_left()
+        cube.show()
+        print()

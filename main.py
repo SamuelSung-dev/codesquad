a33b7b21236e70fbe18fd4aa5b40d98fb8685850
Samuel Sung <SamuelSung.dev@gmail.com>
2020-12-07 20:46:25 +0900

    Feature Quit (Q) function

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from cube2d import Cube2d
 cube = Cube2d()
 
 cube.show()
+
+while True:
+    print("\nCUBE>", end="")
+    input_text = input()
+
+    if input_text == "Q":
+        print("Bye~")
+        break

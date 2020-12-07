4c5ec476e15ef59334ef8818cdfe89f4a80c09a4
Samuel Sung <SamuelSung.dev@gmail.com>
2020-12-07 18:21:03 +0900

    Control input error
    second input is not number or third input is not l(L) or r(R)

--- step-1.py
+++ step-1.py
@@ -3,5 +3,16 @@ input_text = input()
 input_list = input_text.split()
 
 text = input_list[0]
-move_count = input_list[1]
-direction = input_list[2]
+move_count = 0
+if input_list[1].isnumeric():
+    move_count = int(input_list[1])
+else:
+    print("Second input is not number.")
+    exit()
+
+direction = input_list[2].lower()
+if direction != "l" and direction != "r":
+    print("Third input is not l(L) or r(R).")
+    exit()
+
+print(text, move_count, direction)

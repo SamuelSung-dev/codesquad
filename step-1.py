9dadac291601173b2708f4be079df76abc0c73e0
Samuel Sung <SamuelSung.dev@gmail.com>
2020-12-07 19:18:15 +0900

    Complete push word

--- step-1.py
+++ step-1.py
@@ -1,10 +1,10 @@
 input_text = input()
-# print(text)
+
 input_list = input_text.split()
 
 text = input_list[0]
 move_count = 0
-
+is_direction_left = True
 try:
     move_count = int(input_list[1])
 except:
@@ -20,4 +20,22 @@ if direction != "l" and direction != "r":
     print("Third input is not l(L) or r(R).")
     exit()
 
-print(text, move_count, direction)
+if (direction == "l" and move_count > 0) or (direction == "r" and move_count < 0):
+    is_direction_left = True
+else:
+    is_direction_left = False
+
+result = str()
+move = abs(move_count) % len(text)
+
+if move == 0:
+    result = text
+    print(result)
+    exit()
+
+if is_direction_left:
+    result = text[move:len(text)] + text[0:move]
+else:
+    result = text[-move:] + text[0:len(text)-move]
+
+print(result)
